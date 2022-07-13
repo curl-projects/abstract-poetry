3dac2adfd37f62067f6717213521130173577805
finnianmacken <finn@minerva.kgi.edu>
2022-07-13 16:51:30 +1000

    Added tiny TODO

--- app/utils/visited-papers.js
+++ app/utils/visited-papers.js
@@ -78,6 +78,7 @@ export async function getTraversalPath(setter=null){
   return root
 }
 
+// TODO: will we use this?
 export async function checkIfPinned(nodeId){
   const activeNodeId = await localforage.getItem("activeNodeId")
   return activeNodeId === id
