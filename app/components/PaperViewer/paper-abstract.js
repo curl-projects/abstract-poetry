2362af18ece88f50d047ffd6c981d06c2990e7c9
andrepology <andrevacha@gmail.com>
2022-07-14 17:02:29 -0700

    add axis tick, highlights

--- app/components/PaperViewer/paper-abstract.js
+++ app/components/PaperViewer/paper-abstract.js
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react"
+
+
 export function PaperAbstract(props){
 
-  // FUNCTIONALITY
-    // POTENTIAL: Abstracts highlight keywords
+  const [abstract, setAbstract] = useState('')
+
+  useEffect(() => {
+    const keywords  = ["Introduction", "Methods", "Background", "Results", "Discussion", "Conclusion", "Recommendations"];
+    var rawAbstract = props.abstract;
+
+    for (let i = 0; i < keywords.length; i++) {
+      const keyword = keywords[i];
+      const regexp = new RegExp(keyword, "i");
+
+      rawAbstract = rawAbstract.replace(regexp, '<span class="keyword">' + keyword + '</span>');
+    }
+
+    setAbstract("<p>" + rawAbstract + "<h1> {'\u00A0'} </h1>" + "</p>")
+  }, [props.abstract])
+
 
   return(
-    <div className="abstract">
-      {props.abstract ? <p>{props.abstract}</p> : <p>No abstract found for Paper "{props.doi}"</p>}
-      <h1> {'\u00A0'} </h1>
-    </div>
+    <div className="abstract" dangerouslySetInnerHTML={{__html: abstract}}/>
   )
 }
