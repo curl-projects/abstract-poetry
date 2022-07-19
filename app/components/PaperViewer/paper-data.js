22e49e2a8b145c93db89fc5fd45cbc4d5e389b88
andrepology <andrevacha@gmail.com>
2022-07-19 10:27:48 -0700

    fix: added back useEffect in paper-data

--- app/components/PaperViewer/paper-data.js
+++ app/components/PaperViewer/paper-data.js
@@ -8,6 +8,12 @@ export function PaperData(props) {
   const params = useParams();
   const [toggle, setToggle] = useState(false)
 
+  useEffect(() => {
+    if(props.toggle){
+        setToggle(true)
+    }
+  }, [props.toggle])
+
   return (
     <div className="paper-viewer">
 
