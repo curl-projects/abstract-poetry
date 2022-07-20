2fff1e48e7771c91602f99c19c6dc40ff7a62e70
andrepology <andrevacha@gmail.com>
2022-07-20 15:17:57 -0700

    more minor corrections

--- app/components/SeedSearch/search-header.js
+++ app/components/SeedSearch/search-header.js
@@ -4,40 +4,44 @@ import { Form, useParams, Link } from "@remix-run/react"
 
 export function Header() {
   const params = useParams();
-  if(params.paperId){
+  if (params.paperId) {
     return (
       <>
         <div className="header-wrapper" />
         <div className="header flex-space-between">
-            <Link to={'/search'} className="search flex-row" style={{cursor: "pointer", textDecoration: "none"}}>
-              <div className="search-input">
-                <p className="search-prompt">Browse spaces of papers</p>
-                <input type="text" name="searchString" placeholder="abstract poetry" />
+          <Link to={'/search'} className="search flex-row" style={{ cursor: "pointer", textDecoration: "none" }}>
+            <div className="search-input">
+              <p className="search-prompt">Browse spaces of papers</p>
+              <div className="flex-row" style={{ display: "inline-flex" }} >
+                <input type="text" name="searchString" placeholder="abstract poetry " />
+                <button type="submit" style={{ cursor: "pointer" }}>
+                  <img src={glass} alt="Glass Logo" />
+                </button>
               </div>
-              <button type="submit" style={{cursor: "pointer"}}>
-                <img src={glass} alt="Glass Logo" />
-              </button>
-            </Link>
+            </div>
+          </Link>
           <img src={account} alt="Account Login" />
         </div>
       </>
     )
   }
-  else{
+  else {
     return (
       <>
         <div className="header-wrapper" />
         <div className="header flex-space-between">
-            <Form method="post" className="search flex-row">
-              <div className="search-input">
-                <p className="search-prompt">Browse spaces of papers with</p>
-                <input type="text" autoFocus name="searchString" placeholder="abstract poetry" />
+          <Form method="post" className="search flex-row">
+            <div className="search-input">
+              <p className="search-prompt">Browse spaces of papers</p>
+              <div className="flex-row-space-between" style={{ display: "inline-flex", width: "100%" }} >
+                <input type="text" name="searchString" placeholder="abstract poetry " />
+                <button type="submit" style={{ cursor: "pointer" }}>
+                  <img src={glass} alt="Glass Logo" />
+                </button>
               </div>
-              <button type="submit" style={{cursor: "pointer"}}>
-                <img src={glass} alt="Glass Logo" />
-              </button>
-            </Form>
-          <img src={account} alt="Account Login" />
+            </div>
+          </Form>
+          { false? <img src={account} alt="Account Login" /> : null }
         </div>
       </>
     )
