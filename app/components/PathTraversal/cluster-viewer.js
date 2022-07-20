35466bd1656e8753dd2d6192a79d7ffe19ac0c04
andrepology <andrevacha@gmail.com>
2022-07-20 16:51:06 -0700

    update graph styles

--- app/components/PathTraversal/cluster-viewer.js
+++ app/components/PathTraversal/cluster-viewer.js
@@ -26,68 +26,113 @@ export function ClusterViewer(props) {
   }, [containerRef.current])
 
 
-  useEffect(async() => {
+  useEffect(async () => {
     const forceNodeData = JSON.parse(await localStorage.getItem("forceNodes"));
     setGraphData(forceNodeData)
+    console.log(forceNodeData)
   }, [props.forceNodes])
 
-  function setNodeColors(node){
-      if(node.pinned){
-        console.log("NODE.PINNED", node.pinned)
-        return 'blue'
-      }
-      else if(node.nodeId === props.nodeState){
-        return 'rgb(21, 18, 26)'
-      }
-      else if(node.type === 'cluster')
-        return "rgba(183, 176, 183, 0.7)"
-      else {
-        return 'rgba(90, 90, 90, 0.7)'
-      }
+  function setNodeColors(node) {
+    if (node.pinned) {
+      console.log("NODE.PINNED", node.pinned)
+      return 'rgb(178, 171, 243)'
+    }
+    else if (node.nodeId === props.nodeState) {
+      return 'rgb(21, 18, 26)'
+    }
+    else if (node.type === 'cluster')
+      return 'rgb(250,250,250)'
+    else {
+      return 'rgba(90, 90, 90, 0.7)'
+    }
   }
 
+  function setLinkColors(link) {
+    return 'rgb(243,243,243)'
+  }
+
+
   const resolveNodeLabel = useCallback(node => {
-    if(node.title){
+    if (node.title) {
       return node.title
     }
-    else{
+    else {
       return node.name
     }
   }, [fgRef])
 
-  function handleRedirectSubmit(node){
-    if(node.type === 'paper'){
+  function handleRedirectSubmit(node) {
+    if (node.type === 'paper') {
       const formData = new FormData(formRef.current)
       formData.set("redirectString", `/${slugifyDoi(node.doi)}?nodeId=${node.nodeId}`)
       submit(
-          formData,
-          { method: "post", action: "/redirect-cluster-node" }
-        );
+        formData,
+        { method: "post", action: "/redirect-cluster-node" }
+      );
     }
-    else if(node.type === 'cluster'){
+    else if (node.type === 'cluster') {
 
     }
   }
 
+  const getColor = (node) => {
+
+    if (node.pinned) {
+      return 'rgb(178, 171, 243)'
+
+    } else if (node.type === 'paper') {
+      if (node.nodeId === props.nodeState) {
+        return 'rgb(21, 18, 26)'
+      }
+      return 'rgb(238, 238, 238)'
+    }
+    else if (node.type === 'cluster') {
+      return 'rgb(243,243,243)'
+    }
+  }
+
+  const nodePaint = (node, color, ctx) => {
+
+    if (node.type === "cluster") {
+      ctx.fillStyle = 'rgb(255,255,255)';
+    } else if (node.pinned) {
+      ctx.fillStyle = 'rgb(238, 238, 238)';
+    } else if (node.nodeId === props.nodeState) {
+      return 'rgb(100, 100, 100)'
+    } else {
+      ctx.fillStyle = 'rgb(200, 200, 200)';
+    }
+
+    ctx.strokeStyle = color;
+
+    ctx.beginPath();
+    ctx.arc(node.x, node.y, 6, 0, 2 * Math.PI, false);
+    ctx.fill();
+    ctx.stroke();
+  }
+
+
   return (
     <div className="traversal-viewer" ref={containerRef}>
-      <Form ref={formRef} method="post" onSubmit={handleRedirectSubmit}>
+      <Form ref={formRef} method="post" onSubmit={handleRedirectSubmit} />
 
-      </Form>
       {(typeof window !== "undefined") &&
         <ForceGraph2D
           graphData={props.forceNodes ? graphData : { nodes: [], links: [] }}
           ref={fgRef}
           forceEngine="d3"
-          // d3AlphaMin={0}
-          d3AlphaDecay={0.1}
+          d3AlphaMin={0.1}
+          d3AlphaDecay={0.02}
           width={width}
           height={height}
-          cooldownTicks={10}
-          onEngineStop={() => fgRef.current.zoomToFit(1800)}
-          nodeColor={setNodeColors}
+          cooldownTicks={10000}
+          onEngineStop={() => fgRef.current.zoomToFit(400)}
+
+          nodeCanvasObject={(node, ctx) => nodePaint(node, getColor(node), ctx)}
+
           onNodeClick={handleRedirectSubmit}
           nodeLabel={resolveNodeLabel}
+          linkColor={setLinkColors}
         />
       }
     </div>
