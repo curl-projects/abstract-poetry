b5d563444be52f808566a0c0b7fd6d16fd5487b6
finnianmacken <finn@minerva.kgi.edu>
2022-07-23 09:30:41 +1000

    Fixed the JSON parsing bug that Andre found

--- app/components/PaperViewer/paper-metadata.js
+++ app/components/PaperViewer/paper-metadata.js
@@ -14,8 +14,9 @@ export function PaperMetadata(props) {
       // necessary because in search.jsx authors is already parsed into a list,
       // but it's not in paperId
       if(typeof props.metadata.authors === 'string'){
+
         //TODO: this currently removes inner-name hyphens (e.g. D'Souza): come up with a better system later
-        setAuthors(JSON.parse(props.metadata.authors.replace(/([a-zA-Z])+\'([a-zA-Z]+)/g, "$1$2").replace(/'/g, "\"")))
+        setAuthors(JSON.parse(props.metadata.authors.replace("None", "null").replace(/([a-zA-Z])+\'([a-zA-Z]+)/g, "$1$2").replace(/'/g, "\"")))
       }
       else{
         setAuthors(props.metadata.authors)
